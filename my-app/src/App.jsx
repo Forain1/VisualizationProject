101c957afe6bbc0bb54bd2a18625dfd3640c958a
Forain <707585369@qq.com>
2025-05-16 16:49:09 +0800

    init project with dataset

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+import Papa from 'papaparse'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [csvData,setCsvData] = useState([]);
+  useEffect(()=>{
+    fetch('/dataset.csv')
+    .then(res=>res.text())
+    .then((text)=>{
+      Papa.parse(text,{
+        header:true,
+        skipEmptyLines:true,
+        complete:(res,file)=>{
+          setCsvData(res.data);
+        }
+      })
+    })
+  })
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+    <div>
+      <h2>CSV 数据预览：</h2>
+      <pre>{JSON.stringify(csvData, null, 2)}</pre>
+    </div>
+  );
+
 }
 
 export default App
